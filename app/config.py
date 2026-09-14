6ef684e5416cf5a0d751be07b219baa8e7b230ec
ira0109 <130278171+ira0109@users.noreply.github.com>
2026-09-14 12:20:39 +0530

    Removing the Secrets from Config

--- app/config.py
+++ app/config.py
@@ -5,18 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Library Management System"
-    ENVIRONMENT: str = "development"
-    DEBUG: bool = False
-    SECRET_KEY: str = "dev-secret-key-change-me"
-    ADMIN_USERNAME: str = "admin@library.com"
-    ADMIN_PASSWORD: str = "adminpassword"
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    DATABASE_URL: str = "sqlite:///./library.db"
-    DATABASE_SSL_MODE: str = "PREFERRED"
-    CORS_ORIGINS: str = "https://library-management-front-end-gules.vercel.app/"
-
+    
     model_config = SettingsConfigDict(env_file=".env")
 
     @property
